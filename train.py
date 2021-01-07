b2c270e2575fe218255f882d6bbc4a070f17bf45
Bharati <bharatisharada@gmail.com>
2021-01-07 15:16:40 +0000

    Completed Part A HyperDriveConfig run experiment

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
+from azureml.core import Dataset
 from azureml.data.dataset_factory import TabularDatasetFactory
 
 
@@ -39,31 +40,28 @@ def clean_data(data):
     
     return x_df, y_df
 
-### YOUR CODE HERE ###a
-
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-
-url_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-ds = Dataset.Tabular.from_delimited_files(path=url_path)
-print(ds.to_pandas_dataframe())
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-dx_tarin, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
-
-
-run = Run.get_context()
-
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
 
     args = parser.parse_args()
+
+    ### YOUR CODE HERE ###a
+
+    # TODO: Create TabularDataset using TabularDatasetFactory
+    # Data is located at:
+    url_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    ds = Dataset.Tabular.from_delimited_files(path=url_path)
+    print(ds.to_pandas_dataframe())
+
+    x, y = clean_data(ds)
+
+    # TODO: Split data into train and test sets.
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+
+    run = Run.get_context()
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
@@ -71,11 +69,10 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
+    run.log("accuracy", np.float(accuracy))
    
     os.makedirs('./outputs', exist_ok=True)
     joblib.dump(value=model,filename='./outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
-
